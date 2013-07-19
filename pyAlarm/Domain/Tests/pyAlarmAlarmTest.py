9014123211e426faa765f9366fd93558a0f0a567
Riyaan Williams <riyaan.williams@gmail.com>
2013-07-19 12:09:23 +0200

    Found a fix for the UnitTest "package path" issue.

--- pyAlarm/Domain/Tests/pyAlarmAlarmTest.py
+++ pyAlarm/Domain/Tests/pyAlarmAlarmTest.py
@@ -1,8 +1,14 @@
-import datetime
-import unittest
+import datetime, os, sys, unittest
 
-#from Domain.Source.pyAlarmAlarm import Alarm
-#from Domain.Source.pyAlarmAlarmDay import AlarmDay
+"""
+Used this to get the Tests to run.
+The script (python) is looking for a package (Directory) called 'Domain'
+"""
+lib_path = os.path.abspath('../../../pyAlarm')
+sys.path.append(lib_path)
+
+from Domain.Source.pyAlarmAlarm import Alarm
+from Domain.Source.pyAlarmAlarmDay import AlarmDay
 
 class pyAlarmAlarmTest(unittest.TestCase):
     """description of class"""
@@ -11,12 +17,13 @@ class pyAlarmAlarmTest(unittest.TestCase):
         pass
 
     def test_CreateAlarm(self):
-
         alarmTime = datetime.time(5, 0, 0)
 
         alarm = Alarm("Work", "Monday", "elixir", 5, False, alarmTime, True)
 
         self.assertEqual(False, alarm.isRepeatable, "Success")
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    #unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(pyAlarmAlarmTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
